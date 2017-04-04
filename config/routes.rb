b1bc0d81c501a04e0de334ecd869982e1ca28e5c
Johnny Wu <johnnywu@andrew.cmu.edu>
2017-04-03 20:18:13 -0400

    adding root

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :question_scores
   resources :record_scores
   resources :game_records
+
+  root 'application#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
