5f7cd0cfdd2032b24d97b63b6fb5eb6a09ea6d50
Johnny Wu <johnnywu@andrew.cmu.edu>
2017-04-17 12:47:12 -0400

    refactored 2 methods, added is_complete? method for game_record

--- app/models/game_record.rb
+++ app/models/game_record.rb
@@ -10,25 +10,34 @@ class GameRecord < ActiveRecord::Base
 	#considered different from rails generated id??
 	validates_presence_of :gr_id
 	#Methods
-
-	def get_total_score
+	def get_record_score_ids
 		related_record_scores = Record_score.for_game_record(self.gr_id)
 		related_question_scores_id = related_record_scores.map{|n| n.qs_id}
-		#related_question_scores = Array.new
+		related_question_scores_id
+	end
+	def get_total_score
+		related_question_scores_id = get_record_score_ids
 		score = 0
 		related_question_scores_id.length.times do |j|
 			score += Question_score.for_specific(related_question_scores_id[j]).score
 		end
 		score
 	end
-
 	def get_total_time
-		related_record_scores = Record_score.for_game_record(self.gr_id)
-		related_question_scores_id = related_record_scores.map{|n| n.qs_id}
+		related_question_scores_id = get_record_score_ids
 		time = 0
 		related_question_scores_id.length.times do |j|
 			time += Question_score.for_specific(related_question_scores_id[j]).time
 		end
 		time
+	end
+	def is_complete?
+		 related_question_scores_id = get_record_score_ids
+		 related_question_scores_id.length.times do |j|
+		 	if (Question_score.for_specific(related_question_scores_id[j]).visited == false)
+		 		return false
+		 	end
+		 end
+		 return true
 	end
 end
